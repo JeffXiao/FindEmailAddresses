c92f96880a951ed0e5b0b239270ab2bea3cfd881
JeffXiao <jeffx30@gmail.com>
2017-04-06 20:42:37 -0400

    Fixed some warning.

--- src/main/EmailAddresses.java
+++ src/main/EmailAddresses.java
@@ -4,8 +4,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EmailAddresses {
-	public static ArrayList find(String inputString) {
-		ArrayList result = new ArrayList<String>();
+	public static ArrayList<String> find(String inputString) {
+		ArrayList<String> result = new ArrayList<String>();
 		Matcher m = Pattern.compile("[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+").matcher(inputString);
 		while (m.find()) {
 			String email = m.group();
